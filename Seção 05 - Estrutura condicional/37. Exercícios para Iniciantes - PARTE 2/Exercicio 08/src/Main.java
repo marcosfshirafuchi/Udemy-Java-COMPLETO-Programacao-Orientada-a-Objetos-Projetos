632dfe2fe0fd5ea8f68d0195d9aa043ccad88f59
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-14 17:57:59 -0300

    Atualização do exercício 08

--- Seção 05 - Estrutura condicional/37. Exercícios para Iniciantes - PARTE 2/Exercicio 08/src/Main.java
+++ Seção 05 - Estrutura condicional/37. Exercícios para Iniciantes - PARTE 2/Exercicio 08/src/Main.java
@@ -2,6 +2,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    private static final double valorDaIsencao = 2000.00;
+    private static final double valorMaximode8Porcento = 1000.00;
+    private static final double valorMaximode16Porcento = 1500.00;
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
@@ -15,24 +19,26 @@ public class Main {
         if (salario >= 0.00 && salario <= 2000.00) {
             System.out.println("Isento");
         } else if (salario >= 2000.01 && salario <= 3000.00) {
-            calculoDe8Porcento = (salario - 2000.00) * 0.08;
+            calculoDe8Porcento = (salario - valorDaIsencao) * 0.08;
             valorAPagar = calculoDe8Porcento;
+            System.out.printf("R$ %.2f%n",valorAPagar);
 
         } else if (salario >= 3000.01 && salario <= 4500.00) {
-            salario -= 2000.00;
-            restode18Porcento = salario - 1000.00;
+            salario -= valorDaIsencao;
+            restode18Porcento = salario - valorMaximode8Porcento;
             calculoDe8Porcento = (salario - restode18Porcento) * 0.08;
             calculoDe18Porcento = restode18Porcento * 0.18;
             valorAPagar = calculoDe8Porcento + calculoDe18Porcento;
+            System.out.printf("R$ %.2f%n",valorAPagar);
         }else{
-            calculoDe28Porcento = (salario - 4500) * 0.28;
-            restode28Porcento = salario - 4500;
-            calculoDe18Porcento = ((salario - 3000) - restode28Porcento) * 0.18;
-            restode18Porcento = salario - 3000 - restode28Porcento;
-            calculoDe8Porcento = (salario - 2000 - restode18Porcento - restode28Porcento) * 0.08;
+            calculoDe28Porcento = (salario - valorDaIsencao - valorMaximode8Porcento - valorMaximode16Porcento) * 0.28;
+            restode28Porcento = salario - valorDaIsencao - valorMaximode8Porcento - valorMaximode16Porcento;
+            calculoDe18Porcento = ((salario - valorDaIsencao - valorMaximode8Porcento) - restode28Porcento) * 0.18;
+            restode18Porcento = salario - valorDaIsencao - valorMaximode8Porcento - restode28Porcento;
+            calculoDe8Porcento = (salario - valorDaIsencao - restode18Porcento - restode28Porcento) * 0.08;
             valorAPagar = calculoDe8Porcento + calculoDe18Porcento + calculoDe28Porcento;
+            System.out.printf("R$ %.2f%n",valorAPagar);
         }
-        System.out.printf("R$ %.2f%n",valorAPagar);
         scanner.close();
     }
 }
