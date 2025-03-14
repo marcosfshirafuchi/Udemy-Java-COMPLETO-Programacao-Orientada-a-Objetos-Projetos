266d39feb53487a5b5ac08e0c17eaf414099d8ed
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-14 14:10:09 -0300

    Exercício 03

--- Seção 05 - Estrutura condicional/37. Exercícios para Iniciantes - PARTE 2/Exercicio 03/src/Main.java
+++ Seção 05 - Estrutura condicional/37. Exercícios para Iniciantes - PARTE 2/Exercicio 03/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int numero1 = scanner.nextInt();
+        int numero2 = scanner.nextInt();
+        if (numero1 < numero2) {
+            int aux = numero2;
+            numero2 = numero1;
+            numero1 = aux;
+        }
+        if (numero1 % numero2 == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+        scanner.close();
     }
 }
