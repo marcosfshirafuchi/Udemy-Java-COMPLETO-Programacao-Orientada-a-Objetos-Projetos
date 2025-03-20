819ea31bb231578b4e26ce0cacaf51d382ec75da
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-20 20:31:16 -0300

    Exercício 02

---
+++ Seção 06 - Estruturas repetitivas/49. Exercícios para iniciantes - PARTE 3/Exercicio 02/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("primeiro");
+            } else if (x < 0 && y > 0) {
+                System.out.println("segundo");
+            } else if (x < 0 && y < 0) {
+                System.out.println("terceiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("quarto");
+            }
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+
+        }
+        scanner.close();
+    }
+}
