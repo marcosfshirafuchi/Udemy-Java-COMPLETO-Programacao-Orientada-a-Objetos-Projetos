a436c164499c4a5199ac9058b68c68c4fb6a3a48
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-24 15:44:34 -0300

    197. Consumer

---
+++ Seção 18 - Programação funcional e expressões lambda/197. Consumer/Consumer/src/aplication/Program2.java
@@ -0,0 +1,23 @@
+package aplication;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        //Reference method com método estático
+        list.forEach(Product::staticProductPrice);
+        list.forEach(System.out::println);
+    }
+}
---
+++ Seção 18 - Programação funcional e expressões lambda/197. Consumer/Consumer/src/aplication/Program5.java
@@ -0,0 +1,24 @@
+package aplication;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program5 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        double factor = 1.1;
+        //Expressão lambda inline
+        list.forEach(p -> p.setPrice(p.getPrice() * factor));
+
+        list.forEach(System.out::println);
+    }
+}
