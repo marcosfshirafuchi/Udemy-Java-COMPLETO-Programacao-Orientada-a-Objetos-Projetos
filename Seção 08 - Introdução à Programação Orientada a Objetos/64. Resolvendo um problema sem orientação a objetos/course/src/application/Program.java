2b46a2de0b7a565aa86fe93159ef7ad94239e1ce
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-26 13:50:01 -0300

    Atualização do programa

--- Seção 08 - Introdução à Programação Orientada a Objetos/64. Resolvendo um problema sem orientação a objetos/course/src/application/Program.java
+++ Seção 08 - Introdução à Programação Orientada a Objetos/64. Resolvendo um problema sem orientação a objetos/course/src/application/Program.java
@@ -1,7 +1,9 @@
+package application;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
