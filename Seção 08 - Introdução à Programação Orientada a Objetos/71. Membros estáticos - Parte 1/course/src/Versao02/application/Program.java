f4c8a8fcad85713a341acb7fdcc64fc9d9370298
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-04-06 18:06:05 -0300

    71. Membros estáticos - Parte 1

---
+++ Seção 08 - Introdução à Programação Orientada a Objetos/71. Membros estáticos - Parte 1/course/src/Versao02/application/Program.java
@@ -0,0 +1,25 @@
+package Versao02.application;
+
+import Versao02.util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+        double c = cal.circumference(radius);
+        double v = cal.volume(radius);
+        System.out.printf("Circumference: %.2f%n",c);
+        System.out.printf("Volume: %.2f%n",v);
+        System.out.printf("PI value: %.2f%n",cal.PI);
+        sc.close();
+    }
+
+
+}
