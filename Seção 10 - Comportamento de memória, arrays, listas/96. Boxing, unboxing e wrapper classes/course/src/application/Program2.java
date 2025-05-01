6ffdcfecb0caf2aed1333f276b2a8a6165d2158e
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-01 19:37:25 -0300

    96. Boxing, unboxing e wrapper classes

---
+++ Seção 10 - Comportamento de memória, arrays, listas/96. Boxing, unboxing e wrapper classes/course/src/application/Program2.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        int x = 20;
+        //Wrapper Class
+        Integer obj = x;
+
+        System.out.println(obj);
+
+
+        int y = obj * 2;
+
+        System.out.println(y);
+
+    }
+}
