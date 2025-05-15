bddbc0773c313fe619f951fc14d41adaf5531419
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-14 23:55:05 -0300

    124. Exercício resolvido 1 - Parte 2

--- Seção 12 - Enumerações, composição/123. Exercício resolvido 1 - Parte 1/course/src/application/Program.java
+++ Seção 12 - Enumerações, composição/123. Exercício resolvido 1 - Parte 1/course/src/application/Program.java
@@ -2,6 +2,18 @@ package application;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.print("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
 
     }
 }
