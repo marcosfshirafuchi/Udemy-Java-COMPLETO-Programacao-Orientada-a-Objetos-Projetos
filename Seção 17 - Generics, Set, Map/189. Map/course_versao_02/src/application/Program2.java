adcd6d09bb4129a4ac2adc8199955f08257b8326
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-07-09 18:01:01 -0300

    189. Map - Versão 02

---
+++ Seção 17 - Generics, Set, Map/189. Map/course_versao_02/src/application/Program2.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Map<String, String> cookies = new TreeMap<>();
+
+        cookies.put("username", "Maria");
+        cookies.put("email", "maria@gmail.com");
+        cookies.put("phone", "99711122");
+
+        cookies.remove("email");
+        cookies.put("phone", "99711133");
+
+        System.out.println("ALL COOKIES:");
+        for(String key : cookies.keySet()){
+            System.out.println(key + ": " + cookies.get(key));
+        }
+
+    }
+}
