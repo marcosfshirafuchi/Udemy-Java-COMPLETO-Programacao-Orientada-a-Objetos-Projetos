a2cd457be9ca22f8dbb215c50b1121b3227e4a22
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-07-21 20:55:28 -0300

    261. Nivelamento JPA / Hibernate - PARTE 2 (Trabalhando com Maven)

---
+++ Seção 21 - Bônus - Java EE - Mapeamento objeto-relacional com JPA - Hibernate/261. Nivelamento JPA - Hibernate - PARTE 2 (Trabalhando com Maven)/aulajpamaven/src/main/java/com/educandoweb/aulajpamaven/AulajpamavenApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aulajpamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulajpamavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulajpamavenApplication.class, args);
+	}
+
+}
