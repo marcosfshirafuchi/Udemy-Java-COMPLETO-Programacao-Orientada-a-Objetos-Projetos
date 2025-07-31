d27642deade5562b5c42b1fc25951be5079550d3
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-07-30 23:15:44 -0300

    281. Métodos subtotal e total

---
+++ Seção 22 - Bônus - Projeto web services com Spring Boot e JPA - Hibernate/281. Métodos subtotal e total/course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,122 @@
+package com.educandoweb.course.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    //Transient Impede o JPA de interpretar
+    //@Transient
+
+    //Relacionamentos muitos para muitos
+    @ManyToMany
+    //O JoinColumn define o nome da tabela associativa no banco de dados (name)
+    @JoinTable(name = "tb_product_category",
+            //Define o id do product
+            joinColumns = @JoinColumn(name = "product_id"),
+    //Nome da chave estrangeira da tabela category
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    //Depois do id. coloca o nome do atributo (product) que esta classe OrderItemPK
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    //Usa o @JsonIgnore para não dar loop infinito api de Orders
+    @JsonIgnore
+    public Set<Order> orders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+
+        return set;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Product product)) return false;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
+
