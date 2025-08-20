21632190fc0de051eee00fb1b2862b2e95ba466a
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-19 21:58:35 -0300

    342. Obtendo um usuário por id

---
+++ Seção 24 - Bônus - Projeto Spring Boot e banco MongoDB (web services + NoSQL)/342. Obtendo um usuário por id/workshopmongo/src/main/java/com/marcosshirafuchi/workshopmongo/services/UserService.java
@@ -0,0 +1,28 @@
+package com.marcosshirafuchi.workshopmongo.services;
+
+import com.marcosshirafuchi.workshopmongo.domain.User;
+import com.marcosshirafuchi.workshopmongo.repository.UserRepository;
+import com.marcosshirafuchi.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+        if(user.isEmpty()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user.orElse(null);
+    }
+}
