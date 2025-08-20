845055110143160aa67778163cb82f7ff9f37580
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-20 19:16:29 -0300

    343. Inserção de usuário com POST

---
+++ Seção 24 - Bônus - Projeto Spring Boot e banco MongoDB (web services + NoSQL)/343. Inserção de usuário com POST/workshopmongo/src/main/java/com/marcosshirafuchi/workshopmongo/resources/UserResource.java
@@ -0,0 +1,43 @@
+package com.marcosshirafuchi.workshopmongo.resources;
+
+import com.marcosshirafuchi.workshopmongo.domain.User;
+import com.marcosshirafuchi.workshopmongo.dto.UserDTO;
+import com.marcosshirafuchi.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<User> list = service.findAll();
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+}
