5c27b0375b5053115d8a7eeedf88121717b2d55e
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-27 21:37:04 -0300

    382. Passando um objeto Department para o formulário

---
+++ Seção 25 - Bônus (LEGADO) - Interface gráfica com JavaFx (projeto Java 11)/382. Passando um objeto Department para o formulário/workshop-javafx-jdbc/src/main/java/gui/DepartmentListController.java
@@ -0,0 +1,106 @@
+package gui;
+
+import com.marcosfshirafuchi.Main;
+import com.marcosfshirafuchi.model.entities.Department;
+import com.marcosfshirafuchi.model.services.DepartmentService;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class DepartmentListController implements Initializable {
+
+    private DepartmentService service;
+
+    @FXML
+    private TableView<Department> tableViewDepartment;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    private ObservableList<Department> obsList;
+
+    @FXML
+    private void onBtNewAction(ActionEvent event){
+        Stage parentStage = Utils.currentStage(event);
+        Department obj = new Department();
+        createDialogForm(obj,"/gui/DepartmentForm.fxml", parentStage);
+    }
+
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateTableView(){
+        if(service == null){
+            throw new IllegalStateException("Service was null");
+        }
+
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(obsList);
+    }
+
+    private void createDialogForm(Department obj,String absoluteName,Stage parentStage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            DepartmentFormController controller = loader.getController();
+            controller.setDepartment(obj);
+            controller.updateFormData();
+            
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        }
+        catch(IOException e){
+
+            Alerts.showAlert("IOException", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+
+
+}
---
+++ Seção 25 - Bônus (LEGADO) - Interface gráfica com JavaFx (projeto Java 11)/382. Passando um objeto Department para o formulário/workshop-javafx-jdbc/src/main/java/gui/MainViewController.java
@@ -0,0 +1,75 @@
+package gui;
+
+import com.marcosfshirafuchi.Main;
+import com.marcosfshirafuchi.model.services.DepartmentService;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->{
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
+    }
+
+    @FXML
+    public void onMenuItemAboutAction(){
+        loadView("/gui/About.fxml", x -> {});
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private synchronized<T> void loadView(String absoluteName, Consumer<T> initializingAction){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+        }catch (IOException e){
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+}
