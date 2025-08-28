70189ff8836464f4539d6f4c4159b72ff1b1e021
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-28 08:46:35 -0300

    384. Padrão de projeto Observer para atualizar a TableView

---
+++ Seção 25 - Bônus (LEGADO) - Interface gráfica com JavaFx (projeto Java 11)/384. Padrão de projeto Observer para atualizar a TableView/workshop-javafx-jdbc/src/main/java/gui/DepartmentFormController.java
@@ -0,0 +1,115 @@
+package gui;
+
+import com.marcosfshirafuchi.db.DbException;
+import com.marcosfshirafuchi.model.entities.Department;
+import com.marcosfshirafuchi.model.services.DepartmentService;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    private Department entity;
+
+    private DepartmentService service;
+
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    public void setDepartment(Department entity){
+        this.entity = entity;
+    }
+
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
+
+    public void subscribeDataChangeListerner(DataChangeListener listener){
+        dataChangeListeners.add(listener);
+    }
+
+    @FXML
+    public void onBtSaveAction(ActionEvent event){
+        if (entity == null){
+            throw new IllegalStateException("Entity was null");
+        }
+        if (service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            notifyDataChangeListeners();
+            Utils.currentStage(event).close();
+        }
+        catch (DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void notifyDataChangeListeners() {
+        for(DataChangeListener listener : dataChangeListeners){
+            listener.onDataChanged();
+        }
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
+    }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void  updateFormData(){
+        if(entity == null){
+            throw new IllegalStateException("Entity was null");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
+    }
+}
